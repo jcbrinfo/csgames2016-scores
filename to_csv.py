01c2a8d284c85b5f22c360ead6a8b8e6cd645579
jcbrinfo <jcbrinfo@users.noreply.github.com>
2016-03-17 16:18:03 -0400

    to_csv: Use the expected schema
    Signed-off-by: jcbrinfo <jcbrinfo@users.noreply.github.com>

--- to_csv.py
+++ to_csv.py
@@ -18,6 +18,14 @@ from xml.dom.minidom import parseString
 
 IN_EXT = ".html"
 OUT_EXT = ".csv"
+OVERALL_FILENAME = "overall.html"
+REGISTRATION_FILENAME = "registration.html"
+
+class NotFoundError(Exception):
+	"""
+	The requested item was not found.
+	"""
+	pass
 
 class WebPage(object):
 	"""
@@ -66,6 +74,19 @@ class WebPage(object):
 			if cells:
 				yield list(get_inner_text(cell) for cell in cells)
 
+	def get_selected_option(self):
+		"""
+		Return the value of the selected `<option>`.
+
+		Return value
+		------------
+		int
+		"""
+		for option in self.document.getElementsByTagName("option"):
+			if option.hasAttribute("selected"):
+				return option.getAttribute("value")
+		raise NotFoundError("Selected option not found")
+
 def get_inner_text(element):
 	"""
 	Get the inner text of the specified XML element.
@@ -76,7 +97,75 @@ def get_inner_text(element):
 			buffer.append(node.data)
 		elif node.nodeType == node.ELEMENT_NODE:
 			buffer.append(get_inner_text(node))
-	return "".join(buffer)
+	return "".join(buffer).strip()
+
+def list_scores(in_dir_path, out_path, teams):
+	"""
+	Save the scores as a CSV file (data for the `compscores` table).
+
+	Parameters
+	----------
+	in_dir_path : str
+		directory of the web pages
+	out_path : str
+		path to the output file
+	teams : dict of (str, int)
+		mapping between team names and team IDs.
+	"""
+	with open(out_path, "w", newline="") as out_file:
+		for filename in os.listdir(in_dir_path):
+			in_file_path = os.path.join(in_dir_path, filename)
+			if (
+				os.path.isfile(in_file_path)
+				and filename.endswith(IN_EXT)
+				and filename != OVERALL_FILENAME
+				and filename != REGISTRATION_FILENAME
+			):
+				stderr.write("Extracting data from " + in_file_path + "…")
+				writer = csv.writer(out_file)
+				with WebPage(in_file_path) as page:
+					for row in page.get_table_data():
+						writer.writerow((
+							page.get_selected_option(),
+							teams[row[2]],
+							row[3]
+						))
+				stderr.write(" Done.\n")
+
+def list_teams(in_path, out_path):
+	"""
+	Save the list of the teams as a CSV file (data for the `delegations` table).
+
+	Parameters
+	----------
+	in_path : str
+		the web page containing the data
+	out_path : str
+		path to the output file
+
+	Return value
+	------------
+	dict of (str, int)
+		mapping between team names and team IDs.
+	"""
+	with open(out_path, "w", newline="") as out_file:
+		mapping = dict()
+		stderr.write("Extracting data from " + in_path + "…")
+		writer = csv.writer(out_file)
+		with WebPage(in_path) as page:
+			id = 1
+			for row in page.get_table_data():
+				university = row[1]
+				name = row[2]
+				writer.writerow((
+					id, university, None, None, None, None, None, None, None,
+					None, None, None, None, None, None, None, None, name,
+					None, None, None, None, None, None
+				))
+				mapping[name] = id
+				id += 1
+		stderr.write(" Done.\n")
+	return mapping
 
 def run(year):
 	"""
@@ -84,26 +173,18 @@ def run(year):
 	"""
 
 	project_dir = os.path.dirname(os.path.realpath(__file__))
-	in_dir = os.path.join(project_dir, year, "en/")
-	out_dir = os.path.join(project_dir, year, "csv/")
+	year_dir = os.path.join(project_dir, year)
+	in_dir = os.path.join(year_dir, "en")
+	out_dir = os.path.join(year_dir, "csv")
 
 	if not os.path.isdir(out_dir):
 		os.mkdir(out_dir)
 
-	for filename in os.listdir(in_dir):
-		in_file_path = os.path.join(in_dir, filename)
-		if os.path.isfile(in_file_path) and filename.endswith(IN_EXT):
-			stderr.write("Extracting data from " + in_file_path + "…")
-			out_file_path = os.path.join(
-				out_dir,
-				filename[:-len(IN_EXT)] + OUT_EXT
-			)
-			with open(out_file_path, "w", newline="") as out_file:
-				writer = csv.writer(out_file)
-				with WebPage(in_file_path) as page:
-					for row in page.get_table_data():
-						writer.writerow(row)
-			stderr.write(" Done.\n")
+	teams = list_teams(
+		os.path.join(in_dir, REGISTRATION_FILENAME),
+		os.path.join(out_dir, "delegations.csv")
+	)
+	list_scores(in_dir, os.path.join(out_dir, "compscores.csv"), teams)
 
 if __name__ == "__main__":
 	year = argv[1]
